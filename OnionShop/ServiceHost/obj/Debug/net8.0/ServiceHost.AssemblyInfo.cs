bfe17c95969a1d470d7738028b698cc0cf72f31d
sadegh1998 <sadegh.mazaheri1377@gmail.com>
2024-05-23 14:05:28 +0330

    finish slider

--- OnionShop/ServiceHost/obj/Debug/net8.0/ServiceHost.AssemblyInfo.cs
+++ OnionShop/ServiceHost/obj/Debug/net8.0/ServiceHost.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb31090f5e8ed978564368bcd5493e82544280de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+403080aaeb764b85bfdc3a5373ba73f08c84bc12")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServiceHost")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
