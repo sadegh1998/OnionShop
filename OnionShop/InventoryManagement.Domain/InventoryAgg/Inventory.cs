7ba5780c7a93898d5d6ee9399214e37076c8828b
sadegh1998 <sadegh.mazaheri1377@gmail.com>
2024-06-09 23:18:27 +0330

    Add domain class

---
+++ OnionShop/InventoryManagement.Domain/InventoryAgg/Inventory.cs
@@ -0,0 +1,31 @@
+﻿using _0_Framework.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Domain.InventoryAgg
+{
+    public class Inventory : EntityBase
+    {
+        public long ProductId { get; private set; }
+        public int UnitPrice { get; private set; }
+        public bool InStock { get; private set; }
+        public List<InventoryOperation> Operations { get; private set; }
+    }
+
+    public class InventoryOperation
+    {
+        public long Id { get; private set; }
+        public bool Operation { get; private set; }
+        public int Count { get; private set; }
+        public long OperationId { get; private set; }
+        public DateTime OperationDate { get; private set; }
+        public int CurrentCount { get; private set; }
+        public string Description { get; private set; }
+        public int OrederId { get; private set; }
+        public long InventoryId { get; private set; }
+        public Inventory Inventory { get; private set; }
+    }
+}
