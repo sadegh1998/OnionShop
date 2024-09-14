0476bc47334f3f294052d15ec14af2275fdcaa19
sadegh1998 <sadegh.mazaheri1377@gmail.com>
2024-09-14 18:11:30 +0330

    change sms service

--- OnionShop/0-Framework/Application/Sms/SmsService.cs
+++ OnionShop/0-Framework/Application/Sms/SmsService.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using IPE.SmsIrClient;
+using Microsoft.Extensions.Configuration;
 using SmsIrRestful;
 
 namespace _0_Framework.Application.Sms
@@ -11,38 +12,50 @@ namespace _0_Framework.Application.Sms
         {
             _configuration = configuration;
         }
-
         public void Send(string number, string message)
         {
-            var token = GetToken();
-            var lines = new SmsLine().GetSmsLines(token);
-            if (lines == null) return;
-
-            var line = lines.SMSLines.Last().LineNumber.ToString();
-            var data = new MessageSendObject
-            {
-                Messages = new List<string>
-                    {message}.ToArray(),
-                MobileNumbers = new List<string> {number}.ToArray(),
-                LineNumber = line,
-                SendDateTime = DateTime.Now,
-                CanContinueInCaseOfError = true
-            };
-            var messageSendResponseObject = 
-                new MessageSend().Send(token, data);
-
-            if (messageSendResponseObject.IsSuccessful) return;
-
-            line = lines.SMSLines.First().LineNumber.ToString();
-            data.LineNumber = line;
-            new MessageSend().Send(token, data);
-        }
-
-        private string GetToken()
-        {
             var smsSecrets = _configuration.GetSection("SmsSecrets");
-            var tokenService = new Token();
-            return tokenService.GetToken(smsSecrets["ApiKey"], smsSecrets["SecretKey"]);
+
+            SmsIr smsIr = new SmsIr(smsSecrets["ApiKey"]);
+            var responseLines =  smsIr.GetLinesAsync();
+            var line = responseLines.Result.Data.Last();
+            //var response = smsIr.LikeToLikeSend(line, new List<string> { message }.ToArray(), new List<string> { number }.ToArray());
+            smsIr.BulkSend(line,message, new List<string> { number }.ToArray());
+           
         }
+
+
+        //public void Send(string number, string message)
+        //{
+        //    var token = GetToken();
+        //    var lines = new SmsLine().GetSmsLines(token);
+        //    if (lines == null) return;
+
+        //    var line = lines.SMSLines.Last().LineNumber.ToString();
+        //    var data = new MessageSendObject
+        //    {
+        //        Messages = new List<string>
+        //            {message}.ToArray(),
+        //        MobileNumbers = new List<string> {number}.ToArray(),
+        //        LineNumber = line,
+        //        SendDateTime = DateTime.Now,
+        //        CanContinueInCaseOfError = true
+        //    };
+        //    var messageSendResponseObject = 
+        //        new MessageSend().Send(token, data);
+
+        //    if (messageSendResponseObject.IsSuccessful) return;
+
+        //    line = lines.SMSLines.First().LineNumber.ToString();
+        //    data.LineNumber = line;
+        //    new MessageSend().Send(token, data);
+        //}
+
+        //private string GetToken()
+        //{
+        //    var smsSecrets = _configuration.GetSection("SmsSecrets");
+        //    var tokenService = new Token();
+        //    return tokenService.GetToken(smsSecrets["ApiKey"], smsSecrets["SecretKey"]);
+        //}
     }
 }
